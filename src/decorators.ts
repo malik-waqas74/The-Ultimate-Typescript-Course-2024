28d2b4a032a54bc1a3b113e780dbae35857939d0
malik-waqas74 <goodprogrammer619@gmail.com>
2024-07-25 10:11:11 +0500

    changes to decorators files

--- src/decorators.ts
+++ src/decorators.ts
@@ -4,3 +4,4 @@
 // where expression must evaluate to a function that will be called at runtime with 
 // information about the decorated declaration.
 // To create decorators on the setting Experimental Decorators......
+// A decorator is just a function it calls at runtime
